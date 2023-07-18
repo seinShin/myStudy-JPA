f84c81478c8ca860e9b1ef0804b85d6bff7b025e
[seinShin] <tlstpdls13@gmail.com>
2023-07-18 15:18:00 +0900

    DDL 자동 생성 테스트

--- jpa-basic/src/main/java/hello/jpa/JpaMain.java
+++ jpa-basic/src/main/java/hello/jpa/JpaMain.java
@@ -15,16 +15,16 @@ public class JpaMain {
 
         //코드 작성
         try {
-            Member findMember = em.find(Member.class, 1L);
+           //영속
+            Member member = em.find(Member.class, 1L);
+            member.setName("aaaa");
 
-//      delete 쿼리      em.remove(findMember);
-            findMember.setName("HelloJPA");
-
-//            em.persist(member);
-// jpql           List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//            em.detach(member);                영속상태에서 제외 -> 준영속 상태
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)           jpql
 //                            .getResultList();
-            tx.commit();
 
+            //커밋시점에 쿼리를 날림
+            tx.commit();
         } catch (Exception e){
             tx.rollback();
         } finally {
