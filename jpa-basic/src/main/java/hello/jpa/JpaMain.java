743ccc7876d193511fcbeaceb6d418b90e998890
[seinShin] <tlstpdls13@gmail.com>
2023-07-18 15:41:46 +0900

    엔티티 맵핑 테스트

--- jpa-basic/src/main/java/hello/jpa/JpaMain.java
+++ jpa-basic/src/main/java/hello/jpa/JpaMain.java
@@ -15,15 +15,6 @@ public class JpaMain {
 
         //코드 작성
         try {
-           //영속
-            Member member = em.find(Member.class, 1L);
-            member.setName("aaaa");
-
-//            em.detach(member);                영속상태에서 제외 -> 준영속 상태
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)           jpql
-//                            .getResultList();
-
-            //커밋시점에 쿼리를 날림
             tx.commit();
         } catch (Exception e){
             tx.rollback();
